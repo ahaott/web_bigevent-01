bcd2093c4df53142c01f324ea537b51afbafd1de
ahao <1575320267@qq.com>
2020-10-27 19:56:52 +0800

    1

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,10 @@ var baseUrl = 'http://ajax.frontend.itheima.net'
     // 处理参数
 $.ajaxPrefilter(function(options) {
     options.url = baseUrl + options.url
+        // 处理请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.header = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+// 入口函数
+$(function() {
+    // 获取用户信息,渲染用户名和头像
+    getUserInfo()
+})
+
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        headers: {
+            Authorization: localStorage.getItem('token') || ''
+        },
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            // 调用 renderAvatar 渲染用户的头像
+            renderAvatar(res.data)
+        }
+    })
+}
+// 渲染用户头像
+function renderAvatar(user) {
+    var name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    if (user.user_pic !== null) {
+        $('.ayui-nav-img').attr('src', user.user_pic).show()
+        $('.user-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.user-avatar').html(first).show()
+    }
+}
